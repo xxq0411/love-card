e8ef2cc403dc61ff58ef1c2690e1bd252157a53f
xxq0411 <416876921@qq.com>
2018-06-25 23:14:11 +0800

    增加注册表单

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-    <router-view/>
+    <register></register>
   </div>
 </template>
 
 <script>
+import Register from '@/components/Register'
+
 export default {
+  components: {
+    Register
+  },
   name: 'App'
 }
 </script>
---
+++ src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="register">
+    <p>
+      <span>邮箱：</span>
+      <el-input v-model="email" placeholder="请输入邮箱"></el-input>
+    </p>
+    <p>
+      <span>密码：</span>
+      <el-input type="password" v-model="password1" placeholder="请输入密码"></el-input>
+    </p>
+    <p>
+      <span>确认密码：</span>
+      <el-input type="password" v-model="password2" placeholder="请确认密码"></el-input>
+    </p>
+    <el-button type="primary" @click="submit">提交</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password1: '',
+      password2: ''
+    }
+  },
+  methods: {
+    submit: function () {
+      if (this.email === '') {
+        this.$message.error('记得填写邮箱哦')
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+  .register {
+    padding:20px;
+  }
+  .register >p >span {
+    display:inline-block;
+    width:80px;
+  }
+  .el-input {
+    width:30%
+  }
+</style>
